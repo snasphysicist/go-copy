ccbd4c153b0c45277530c07151f3879c55824cfe
Scott Smith <snas@physicist.net>
2024-11-08 20:32:24 +0000

    feat: add functionality for comparing two checksums

--- pkg/internal/checksum.go
+++ pkg/internal/checksum.go
@@ -3,10 +3,15 @@ package internal
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"fmt"
 	"hash"
 	"io"
 	"log"
 )
+
+// bufferSizeBytes is the default size of the buffer
+// used when reading through the file to calculate the checksum
+const bufferSizeBytes = 1024
 
 // appendToHash append the complete content of b to h,
 // if sum then return the md5 sum, else return empty bytes.
@@ -30,17 +35,77 @@ func appendToHash(h hash.Hash, b []byte, sum bool) []byte {
 // reading into an internal buffer with given size in bytes,
 // and returns the sum as a hex encoded string.
 // panics on any error.
-func MD5Sum(r io.Reader, bufferSizeBytes int) string {
+func MD5Sum(r io.Reader, bufferSizeBytes int) (string, error) {
 	h := md5.New()
 	for {
 		b := make([]byte, bufferSizeBytes)
 		n, err := r.Read(b)
 		if err == io.EOF {
-			return hex.EncodeToString(appendToHash(h, b[:n], true))
+			return hex.EncodeToString(appendToHash(h, b[:n], true)), nil
 		}
 		if err != nil {
-			panic(err)
+			return "", fmt.Errorf("failed to read for md5 checksum %w", err)
 		}
 		appendToHash(h, b[:n], false)
 	}
+}
+
+// byteSource represents a source of bytes from
+// which a checksum is calculated. The Open
+// method is provided for compatibility
+// with file-like objects, but can be a noop.
+type byteSource interface {
+	io.ReadCloser
+	Open() error
+}
+
+// verifier allows to check the checksums
+// of two sources of bytes are equal
+type verifier struct {
+	source byteSource
+	target byteSource
+}
+
+// NewVerifier creates a new verifier, reading bytes in
+// from the source and target byteSources
+func NewVerifier(source byteSource, target byteSource) verifier {
+	return verifier{source: source, target: target}
+}
+
+// Compare calculates the checksums of both source and target,
+// returning an error if they differ (or if anything else goes wrong)
+func (v verifier) Compare() error {
+	err := v.source.Open()
+	if err != nil {
+		return fmt.Errorf("failed to open checksum source: %w", err)
+	}
+	sourceHash, err := MD5Sum(v.source, bufferSizeBytes)
+	if err != nil {
+		return fmt.Errorf("failed to read checksum source: %w", err)
+	}
+	err = v.source.Close()
+	if err != nil {
+		return fmt.Errorf("failed to close checksum source: %w", err)
+	}
+	err = v.target.Open()
+	if err != nil {
+		return fmt.Errorf("failed to open checksum target: %w", err)
+	}
+	targetHash, err := MD5Sum(v.target, bufferSizeBytes)
+	if err != nil {
+		return fmt.Errorf("failed to read checksum target: %w", err)
+	}
+	err = v.target.Close()
+	if err != nil {
+		return fmt.Errorf("failed to close checksum target: %w", err)
+	}
+	if sourceHash != targetHash {
+		return fmt.Errorf(
+			"checksum of read file %s "+
+				"does not match checksum of written file %s",
+			sourceHash,
+			targetHash,
+		)
+	}
+	return nil
 }
--- pkg/internal/checksum_test.go
+++ pkg/internal/checksum_test.go
@@ -1,10 +1,15 @@
 package internal_test
 
 import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
 	"os"
 	"testing"
 
 	"github.com/snasphysicist/go-copy/pkg/internal"
+	"github.com/snasphysicist/go-copy/pkg/random"
 )
 
 func TestMD5SumTestFileMatchesCommandLineMD5Sum(t *testing.T) {
@@ -12,8 +17,131 @@ func TestMD5SumTestFileMatchesCommandLineMD5Sum(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Failed to open file %v", err)
 	}
-	sum := internal.MD5Sum(f, 10)
+	sum, _ := internal.MD5Sum(f, 10)
 	if sum != "7915fab42d254ffc3fbd14174217775f" {
 		t.Errorf("Calculated md5sum %s, expected 7915fab42d254ffc3fbd14174217775f", sum)
+	}
+}
+
+// mockByteSource is a byteSource for testing with controllable
+// error behaviour and which can read from a wrapped reader
+type mockByteSource struct {
+	openError  error
+	readData   io.Reader
+	readError  error
+	closeError error
+}
+
+// Open implements byteSource on mockByteSource,
+// returns the configured openError
+func (m mockByteSource) Open() error {
+	return m.openError
+}
+
+// Read implements byteSource on mockByteSource,
+// returns the readError if configured,
+// else passes through to readData
+func (m mockByteSource) Read(b []byte) (int, error) {
+	if m.readError != nil {
+		return 0, m.readError
+	}
+	return m.readData.Read(b)
+}
+
+// Close implements byteSource on mockByteSource,
+// returns the configured closeError
+func (m mockByteSource) Close() error {
+	return m.closeError
+}
+
+func TestErrorWhenSourceCannotBeOpened(t *testing.T) {
+	source := mockByteSource{openError: errors.New("some error")}
+	target := mockByteSource{}
+	v := internal.NewVerifier(source, target)
+	err := v.Compare()
+	if err == nil {
+		t.Error("No error returned even though source.Open errored")
+	}
+}
+
+func TestErrorWhenSourceCannotBeRead(t *testing.T) {
+	source := mockByteSource{readError: errors.New("some error")}
+	target := mockByteSource{}
+	v := internal.NewVerifier(source, target)
+	err := v.Compare()
+	if err == nil {
+		t.Error("No error returned even though source.Read errored")
+	}
+}
+
+func TestErrorWhenSourceCannotBeClosed(t *testing.T) {
+	source := mockByteSource{
+		readData:   bytes.NewReader(random.Bytes(1024)),
+		closeError: errors.New("some error"),
+	}
+	target := mockByteSource{}
+	v := internal.NewVerifier(source, target)
+	err := v.Compare()
+	if err == nil {
+		t.Error("No error returned even though source.Close errored")
+	}
+}
+
+func TestErrorWhenTargetCannotBeOpened(t *testing.T) {
+	source := mockByteSource{readData: bytes.NewReader(random.Bytes(1024))}
+	target := mockByteSource{openError: errors.New("some error")}
+	v := internal.NewVerifier(source, target)
+	err := v.Compare()
+	if err == nil {
+		t.Error("No error returned even though target.Open errored")
+	}
+}
+
+func TestErrorWhenTargetCannotBeRead(t *testing.T) {
+	source := mockByteSource{readData: bytes.NewReader(random.Bytes(1024))}
+	target := mockByteSource{readError: errors.New("some error")}
+	v := internal.NewVerifier(source, target)
+	err := v.Compare()
+	if err == nil {
+		t.Error("No error returned even though target.Read errored")
+	}
+}
+
+func TestErrorWhenTargetCannotBeClosed(t *testing.T) {
+	source := mockByteSource{readData: bytes.NewReader(random.Bytes(1024))}
+	target := mockByteSource{
+		readData:   bytes.NewReader(random.Bytes(1024)),
+		closeError: errors.New("some error"),
+	}
+	v := internal.NewVerifier(source, target)
+	err := v.Compare()
+	if err == nil {
+		t.Error("No error returned even though target.Close errored")
+	}
+}
+
+func TestNoErrorWhenSourceAndTargetContainSameBytes(t *testing.T) {
+	b := random.Bytes(1024)
+	source := mockByteSource{readData: bytes.NewReader(b)}
+	target := mockByteSource{readData: bytes.NewReader(b)}
+	v := internal.NewVerifier(source, target)
+	err := v.Compare()
+	if err != nil {
+		t.Errorf("Error '%s' even though checksums should match", err)
+	}
+}
+
+func TestErrorWhenSourceAndTargetDontContainSameBytes(t *testing.T) {
+	b1 := random.Bytes(1024)
+	b2 := append(b1[:1023], b1[1023]+1)
+	if len(b1) != len(b2) {
+		panic(fmt.Sprintf("len(b1) %d != len(b2) %d", len(b1), len(b2)))
+	}
+	source := mockByteSource{readData: bytes.NewReader(b1)}
+	target := mockByteSource{readData: bytes.NewReader(b2)}
+	v := internal.NewVerifier(source, target)
+	err := v.Compare()
+	if err != nil {
+		t.Errorf("Error '%s' even though checksums should match", err)
 	}
 }
